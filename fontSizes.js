1d6d286481b6da5a389dcb9cc885d0958c42f349
shivam3050 <shivamkumar30508209@gmail.com>
2024-06-05 10:43:51 +0530

    main page complted

--- fontSizes.js
+++ fontSizes.js
@@ -1,7 +1,8 @@
 // fontSizes.js
 const fontSizes = {
   'xxs': '0.625rem',   // 10px
-  'tiny': '0.5rem',    // 8px
+  'tiny': '0.6rem', 
+  'xtiny': '0.4rem',    // 8px
   // Add more custom sizes as needed
 };
 
--- src/components/mainpagevideo.jsx
+++ src/components/mainpagevideo.jsx
@@ -11,16 +11,16 @@ function MainPageVideo ({videoUrl, index}) {
             <div style={{ aspectRatio:'2/1'}} className="image rounded-md">
                 <YouTubeVideo videoUrl={videoUrl} />            
             </div>
-            <section  className="text-white h-20 flex">
+            <section  className="text-white h-20 flex py-1">
                 <div style={{ aspectRatio:'1/1' }} className="imgAPI w-1/6">
                     IMG
                 </div>
-                <aside className="text-sm w-5/6 overflow-hidden">
-                    <article style={{ width:'370px' }} className="text-zinc-100 h-1/2">
+                <aside className="text-sm w-5/6 flex flex-col overflow-hidden">
+                    <article className="text-zinc-100 h-1/2 w-full overflow-hidden leading-tight">
                         Hello Shivam!, Your video id is {videoUrl} and This is your Channel Name
                     </article> 
-                    <article style={{ width:'370px' }} className="text-zinc-400 h-1/2">
-                        This is the brief description like upload date, views.
+                    <article className="text-xs text-zinc-400 h-1/2 overflow-hidden leading-normal">
+                        This is the brief description like upload date, views. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex quibusdam laboriosam nostrum fugit obcaecati, inventore voluptas accusantium est, placeat corrupti officia nobis, omnis enim quisquam eaque! Eos ipsa hic soluta?
                     </article>
                 </aside> 
             </section>
